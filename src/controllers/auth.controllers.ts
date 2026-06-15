7c69f60af4d9d8ae28175e7dfa9cbd0681e6e006
Carol Barbieri <carolinabarbieri290508@gmail.com>
2026-06-15 08:39:19 -0300

    efetuando modelo

--- src/controllers/auth.controllers.ts
+++ src/controllers/auth.controllers.ts
@@ -1,13 +1,62 @@
 import { RequestHandler } from "express"
+import { z } from "zod"
+import { createUser, verifyUser } from '../services/user.service'
+import { createToken } from "../services/auth.service"
 
-export const signIn: RequestHandler= (req, res) => {
-    // Lógica para autenticar o usuário
+export const signIn: RequestHandler = async (req, res) => {
+const schema = z.object({
+name: z.string(),
+email: z.string().email(),
+password: z.string()
+})
+const data = schema.safeParse(req.body)
+if (!data.success) {
+return res.status(400).json({ error: data.error?.flatten().fieldErrors })
+}
+const user = await verifyUser(data.data)
+if (!user) {
+return res.status(400).json({ error: 'Email ou senha inválidos' })
+}
+const token = createToken(user)
+res.json({
+message: 'Login bem-sucedido',
+user: { 
+id: user.id,
+name: user.name,
+email: user.email
+},
+token
+})
+
 }
 
-export const signUp: RequestHandler= (req, res) => {
-    // Lógica para registrar um novo usuário
+export const signUp: RequestHandler = async (req, res) => {
+const schema = z.object({
+name: z.string(),
+email: z.string().email(),
+password: z.string()
+})
+const data = schema.safeParse(req.body)
+if (!data.success) {
+return res.status(400).json({ error: data.error?.flatten().fieldErrors })
+}
+const newUser = await createUser(data.data)
+if (!newUser) {
+return res.status(400).json({ error: 'Email já cadastrado' })
 }
 
-export const validate: RequestHandler= (req, res) => {
-    // Lógica para validar o token de autenticação
+const token = createToken(newUser)
+res.status(201).json({
+message: 'Usuário criado com sucesso',
+user: {
+id: newUser.id,
+name:newUser.name,
+email: newUser.email
+},
+token
+})
+}
+
+export const validate: RequestHandler = (req, res) => {
+// Lógica para validar o token de autenticação
 }
